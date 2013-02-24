b98320f9b9f11da36869feb0f6a44284dfeeb897
Alma Mae J. Bernales <maebernales@gmail.com>
2013-02-24 23:24:06 +0800

    cof rails folder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+COF::Application.config.secret_token = 'b518f146283270c019e91a1838ac16d2fe42bfd0e30faf3a358022b012fa5de56415287183e8b65d48c8bd5979b7f532b1cd27c2b4190cb99c44d1d3f7028c8a'
